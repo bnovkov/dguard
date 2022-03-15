0bef4c708c87b1eeb9c634763e714ca1afa0d5c9
Bojan Novković <bojan.novkovic@fer.hr>
2022-03-15 10:33:35 +0100

    Alloca iter

--- src/pass.cpp
+++ src/pass.cpp
@@ -33,6 +33,7 @@
 #include "pass.h"
 
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Passes/PassBuilder.h"
 
@@ -44,71 +45,19 @@ using namespace llvm;
 // InjectFuncCall implementation
 //-----------------------------------------------------------------------------
 bool DOPGuard::runOnModule(Module &M) {
-  bool InsertedAtLeastOnePrintf = false;
+    for(auto &Func : M){
+        for(auto &BB : Func){
+          llvm::SmallVector<PointerType> *ptrVars = new llvm::SmallVector<PointerType>();
+          llvm::SmallVector<Instruction> *allocas = new llvm::SmallVector<Instruction>();
 
-  auto &CTX = M.getContext();
-  PointerType *PrintfArgTy = PointerType::getUnqual(Type::getInt8Ty(CTX));
-
-  // STEP 1: Inject the declaration of printf
-  // ----------------------------------------
-  // Create (or _get_ in cases where it's already available) the following
-  // declaration in the IR module:
-  //    declare i32 @printf(i8*, ...)
-  // It corresponds to the following C declaration:
-  //    int printf(char *, ...)
-  FunctionType *PrintfTy = FunctionType::get(
-      IntegerType::getInt32Ty(CTX),
-      PrintfArgTy,
-      /*IsVarArgs=*/true);
-
-  FunctionCallee Printf = M.getOrInsertFunction("printf", PrintfTy);
-
-  // Set attributes as per inferLibFuncAttributes in BuildLibCalls.cpp
-  Function *PrintfF = dyn_cast<Function>(Printf.getCallee());
-  PrintfF->setDoesNotThrow();
-  PrintfF->addParamAttr(0, Attribute::NoCapture);
-  PrintfF->addParamAttr(0, Attribute::ReadOnly);
-
-
-  // STEP 2: Inject a global variable that will hold the printf format string
-  // ------------------------------------------------------------------------
-  llvm::Constant *PrintfFormatStr = llvm::ConstantDataArray::getString(
-      CTX, "(llvm-tutor) Hello from: %s\n(llvm-tutor)   number of arguments: %d\n");
-
-  Constant *PrintfFormatStrVar =
-      M.getOrInsertGlobal("PrintfFormatStr", PrintfFormatStr->getType());
-  dyn_cast<GlobalVariable>(PrintfFormatStrVar)->setInitializer(PrintfFormatStr);
-
-  // STEP 3: For each function in the module, inject a call to printf
-  // ----------------------------------------------------------------
-  for (auto &F : M) {
-    if (F.isDeclaration())
-      continue;
-
-    // Get an IR builder. Sets the insertion point to the top of the function
-    IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
-
-    // Inject a global variable that contains the function name
-    auto FuncName = Builder.CreateGlobalStringPtr(F.getName());
-
-    // Printf requires i8*, but PrintfFormatStrVar is an array: [n x i8]. Add
-    // a cast: [n x i8] -> i8*
-    llvm::Value *FormatStrPtr =
-        Builder.CreatePointerCast(PrintfFormatStrVar, PrintfArgTy, "formatStr");
-
-    // The following is visible only if you pass -debug on the command line
-    // *and* you have an assert build.
-    LLVM_DEBUG(dbgs() << " Injecting call to printf inside " << F.getName()
-                      << "\n");
-
-    // Finally, inject a call to printf
-    Builder.CreateCall(
-        Printf, {FormatStrPtr, FuncName, Builder.getInt32(F.arg_size())});
-
-    InsertedAtLeastOnePrintf = true;
-  }
-
-  return InsertedAtLeastOnePrintf;
+          for (auto &inst : BB) {
+            if(AllocaInst *al = dyn_cast<AllocaInst *>(inst)){
+              
+            }
+          }
+        }
+    }
+  
 }
 
 PreservedAnalyses DOPGuard::run(llvm::Module &M,
