7a6fb01767f290c956d2f2ccf73cf4b9ee7ca724
SirSleepsA-Lot <antoniomocinic@gmail.com>
2022-12-02 17:33:12 +0100

    printanje grafova i pocetak labeliranja

--- labeling/main.py
+++ labeling/main.py
@@ -1,18 +1,7 @@
-from group import Group
-from element import Element
-from random import sample
+from group import System
+
 
 if __name__=='__main__':
-    groups = [Group(i, []) for i in range(5)]
-    for i in range(10):
-        element = Element(i+6)
-        groupId = sample(range(0, 5), 2)
-        for j in groupId:
-            groups[j].add(element)
-    for group in groups:
-        print(group)
-    
-
-
-
+    system = System(10, 3)
+    print(system)
     
